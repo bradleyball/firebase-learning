fe2dfd1ef117b050112929a2aea1656c0acc548c
cacheUp <bradb26@gmail.com>
2019-01-20 21:45:12 +0300

    modified methods, cleaned up code

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -2,51 +2,34 @@ import React, { Component } from "react";
 import { firestore } from "../firebase";
 import Posts from "./Posts";
 import { collectIdsAndData } from "../utilities";
+import Authentication from "./Authentication";
 class Application extends Component {
   state = {
-    posts: []
+    posts: [],
+    user: null
   };
 
   unsubscribe = null; // NEW
 
   componentDidMount = async () => {
     this.unsubscribe = firestore.collection("posts").onSnapshot(snapshot => {
-      // NEW
       const posts = snapshot.docs.map(collectIdsAndData);
       this.setState({ posts });
     });
   };
 
   componentWillUnmount = () => {
-    // NEW
     this.unsubscribe();
-  };
-  //using add to put the new post in data base and generate a new id
-  //set doc as a variable to get the post back from the database
-  //set new post and call the getter function inside our collect Ids and docs function
-  //to retrieve the data and then
-  handleCreate = async post => {
-    firestore.collection("posts").add(post);
-  };
-  //set allPosts to the array of posts
-  //filters out the post if the id of the post matches the current id of the event
-  //the await function goes into the posts grabs the id and uses the .delete method
-  //sets the state back to the remaining posts
-  handleRemove = async id => {
-    firestore.doc(`posts/${id}`).delete();
   };
 
   render() {
-    const { posts } = this.state;
+    const { posts, user } = this.state;
 
     return (
       <main className="Application">
         <h1>Think Piece</h1>
-        <Posts
-          posts={posts}
-          onCreate={this.handleCreate}
-          onRemove={this.handleRemove}
-        />
+        <Authentication user={user} />
+        <Posts posts={posts} />
       </main>
     );
   }
